d82acea71e00ed2fb88760f22e5e3fcca1e7d5a8
David <admin@Davids-MacBook-Pro.local>
2021-02-08 16:28:46 +0000

    backend API setup complete

--- server/server.js
+++ server/server.js
@@ -39,35 +39,46 @@ app.get("/api/v1/restaurants/:id", async (req, res) => {
 })
 
 // Create a restaurant
-app.post("/api/v1/restaurants", (req, res) => {
-  console.log(req.body)
-
-  res.status(201).json({
-    status: "success",
-    data: {
-      restaurant: "subway"
-    }
-  })
+app.post("/api/v1/restaurants", async (req, res) => {
+  try {
+    const results = await db.query("INSERT INTO restaurants (name, location, price_range) values ($1, $2, $3) returning *", [req.body.name, req.body.location, req.body.price_range])
+    console.log(results)
+    res.status(201).json({
+      status: "success",
+      data: {
+        restaurant: results.rows[0]
+      }
+    })
+  } catch (err) {
+    console.log(err)
+  }
 })
 
 // Update a restaurant
-app.put("/api/v1/restaurants/:id", (req, res) => {
-  console.log(req.params.id)
-  console.log(req.body)
-
-  res.status(200).json({
-    status: "success",
-    data: {
-      restaurant: "subway"
-    }
-  })
+app.put("/api/v1/restaurants/:id", async (req, res) => {
+  try {
+    const results = await db.query("UPDATE restaurants SET name = $1, location = $2, price_range = $3 where id = $4 returning *", [req.body.name, req.body.location, req.body.price_range, req.params.id])
+    res.status(200).json({ 
+      status: "success",
+      data: {
+        restaurant: results.rows[0]
+      }
+    })
+  } catch (err) {
+    console.log(err)
+  }
 })
 
 // Delete a restaurant
-app.delete("/api/v1/restaurants/:id", (req, res) => {
-  res.status(204).json({
-    status: "success"
-  })
+app.delete("/api/v1/restaurants/:id", async (req, res) => {
+  try {
+    const results = await db.query("DELETE FROM restaurants WHERE id = $1", [req.params.id])
+    res.status(204).json({
+      status: "success"
+    })
+  } catch (err) {
+    console.log(err)
+  }
 })
 
 const port = process.env.PORT || 3002
